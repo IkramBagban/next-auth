d0acd4496e8803273683266269378f97f7596eec
ikramBagban <107988060+IkramBagban@users.noreply.github.com>
2024-05-12 14:28:44 +0530

    setup project

---
+++ src/dbConfig/dbConfig.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+export async function connect() {
+    try {
+
+        mongoose.connect(process.env.MONGO_URI!)
+        const connection = mongoose.connection;
+
+        connection.on('connected', ()=>{
+            console.log("Database connected successfully")
+        })
+
+        connection.on('error', (err)=>{
+            console.log("Mongodb connection Failed.", err)
+            process.exit();
+        })
+        
+    } catch (error) {
+        console.log('Something went wrong')
+        console.log('error', error)
+        
+    }
+}
