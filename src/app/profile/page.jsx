a1debb1912ab6820affa1eb048fff55fd0092db9
ikramBagban <107988060+IkramBagban@users.noreply.github.com>
2024-05-14 16:51:08 +0530

    use dynamic route & get the value

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,11 +1,13 @@
 "use client";
 
 import axios from "axios";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 
 const ProfilePage = () => {
     const router = useRouter();
+    const [data, setData] = useState(null)
 
     const logoutHandler = async () => {
         try {
@@ -21,15 +23,36 @@ const ProfilePage = () => {
         }
     }
 
+    const getUserDetails  = async ()=>{
+        const res= await axios.get('/api/users/me')
+        console.log(res.data)
+        setData(res.data.data._id)
+    }
+
     return (
-        <div className="flex items-center justify-center min-h-screen bg-gray-900">
-            <h2>
-                Profile Page
-            </h2>
-            
-            <button onClick={logoutHandler} className="border p-1 m-1 rounded hover:bg-white hover:text-black">Logout</button>
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white">
+          <h2 className="text-3xl font-semibold mb-6">Profile Page</h2>
+          <p className="mb-4">
+            {data ? (
+              <Link href={`/profile/${data}`} className="text-blue-500">{data}</Link>
+            ) : (
+              'Nothing'
+            )}
+          </p>
+          <button
+            onClick={logoutHandler}
+            className="bg-red-500 hover:bg-red-600 text-white font-semibold py-2 px-4 rounded-lg mb-2"
+          >
+            Logout
+          </button>
+          <button
+            onClick={getUserDetails}
+            className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-lg"
+          >
+            Get Data
+          </button>
         </div>
-    );
-};
+      );
+    };
 
 export default ProfilePage
